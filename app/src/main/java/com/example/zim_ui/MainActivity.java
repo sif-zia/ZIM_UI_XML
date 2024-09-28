ffe9f43a02e1e267a1e1bfdebfb166efe4e19883
Zainab Bilal <zainabilalmunawar@gmail.com>
2024-09-28 21:01:59 +0500

    home page

--- app/src/main/java/com/example/zim_ui/MainActivity.java
+++ app/src/main/java/com/example/zim_ui/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_signup);
 
 
     }
